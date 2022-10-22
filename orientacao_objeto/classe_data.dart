3be4f36531cdc0135ababe84838c8b0529ac7461
vianajulio <juliogabrielv8@gmail.com>
2022-10-22 17:32:49 -0300

    Adicionanto tipos de construtores

--- orientacao_objeto/classe_data.dart
+++ orientacao_objeto/classe_data.dart
@@ -3,6 +3,28 @@ class Data {
   int? mes;
   int? ano;
 
+  //Construtores
+
+  // Data(int dia, int mes, int ano) {
+  //   this.dia = dia;
+  //   this.mes = mes;
+  //   this.ano = ano;
+  // }
+
+  // Data([this.dia =1 , this.mes = 2, this.ano = 1990]); colchetes deixam os parâmetros do construtor a serem usados opcionalmente,
+  //e é possível definir um valor padrão, caso nada seha inserido.
+
+  // Construtores nomeados:
+
+  // Data({this.dia, this.mes, this.ano}); colchetes deixam os parâmetros do construtor a serem usados opcionalmente.
+
+  // Construtor com parâmetros obrigatórios
+  Data(this.dia, this.mes, this.ano);
+
+  // Construtor nomeado
+  Data.com({this.dia = 1, this.mes = 2, this.ano = 1990});
+
+
   String dataFormatada() {
     return '$dia/$mes/$ano';
   }
@@ -13,20 +35,13 @@ class Data {
 }
 
 main() {
-  var dataAniversario = new Data();
-  dataAniversario.dia = 13;
-  dataAniversario.mes = 10;
-  dataAniversario.ano = 2003;
-
+  var dataAniversario = new Data(13, 10, 2003);
+  
   print('Data de nascimento: ${dataAniversario.dataFormatada()}');
-  // print('Data de nascimneto: ${dataAniversario.dia}/${dataAniversario.mes}/${dataAniversario.ano}');
 
-  Data dataCompra = Data();
-  dataCompra.dia = 22;
-  dataCompra.mes = 10;
-  dataCompra.ano = 2022;
-
-  print('Data da compra: ${dataCompra.dataFormatada()}');
-  // print('Data da compra: ${dataCompra.dia}/${dataCompra.mes}/${dataCompra.ano}');
+  Data dataCompra = Data.com(dia: 22, mes: 10, ano: 2022);
   print(dataCompra.toString());
+
+  var dataFinal = Data.com(dia: 22, mes: 10, ano: 2022);
+  print('Data de exemplo: $dataFinal');
 }
