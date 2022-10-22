5cedb5f830c62462f14ceee2eebcd91ca4a2789a
vianajulio <juliogabrielv8@gmail.com>
2022-10-22 20:59:07 -0300

    Adicionando novamente a pasta filter function

---
+++ filter_function/filter.dart
@@ -0,0 +1,14 @@
+void main() {
+  var notas = [8.2, 7.1, 6.2, 4.4, 3.9, 8.8, 9.1, 5.1];
+
+  var notasBoasFn = (double nota) => nota >= 7;
+  var notasMuitoBoasFn = (double nota) => nota >= 8.8;
+
+  var notasBoas = notas.where(notasBoasFn);
+  var notasMuitoBoas = notas.where(notasMuitoBoasFn);
+
+  // print para mostrar as notas
+  print("Notas: $notas");
+  print("Notas boas: $notasBoas");
+  print("Notas muito boas: $notasMuitoBoas");
+}
---
+++ filter_function/filter_02.dart
@@ -0,0 +1,24 @@
+//Função do tipo genérica, que faz a filtragem de elementos passado por seus parâmetros.
+List<E> filtrar<E>(List<E> lista, bool Function(E) fn) {
+  List<E> listaFiltrada = [];
+  for (E elemento in lista) {
+    if (fn(elemento)) {
+      listaFiltrada.add(elemento);
+    }
+  }
+  return listaFiltrada;
+}
+
+main() {
+  var notas = [8.2,7.3,6.8,5.4,2.7,9.3];
+  //cria-se uma variável passando uma arrow function para ela, e faz uma validação.
+  var notasBoasFn = (double nota) => nota >= 7.5;
+  //no método filtrar pegará os dados da lista "notas" e fará a comparação utilizando a variável "notasBoasFn"
+  var somenteNotasBoas = filtrar(notas, notasBoasFn);
+  print(somenteNotasBoas);
+
+  // realizando mais testes.
+  var nomes = ['Ana', 'Bia', 'Rebeca', 'Gui', 'João']; 
+  var nomesGrandesFn = (String nome) => nome.length >= 4;
+  print(filtrar(nomes, nomesGrandesFn));
+}
